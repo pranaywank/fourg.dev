fe3d1de0800fdd09daca0527a7a964160c6ce220
Pranay Wankhede <pranaywankhede@Pranays-MacBook-Pro.local>
2026-04-17 15:10:44 +0530

    Fix github pages 404 with trailingSlash and nojekyll

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/fourg.dev",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
